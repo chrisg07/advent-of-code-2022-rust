12a3a28c14ec513221939e2978a380b6b8725f80
Christopher Guilliams <chrisg07@vt.edu>
2022-12-08 22:25:14 -0600

    reading input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    let contents = fs::read_to_string("src/test-case-input.txt")
+        .expect("Should have been able to read the file");
+
+    let lines = contents.lines();
+    for line in lines {
+        for char in line.chars() {
+
+            println!("{}", char)
+        }
+    }
+    println!("With text:\n{contents}");
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(4, 4);
 }
