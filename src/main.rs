0fddef3f10f8d53352f5296e76224f0a3af3a13c
Christopher Guilliams <chrisg07@vt.edu>
2022-12-10 15:25:55 -0600

    Day 10 Par A

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,104 @@
-use std::{fs, collections::HashSet};
+use std::{fs, collections::{HashSet, VecDeque}};
 
 fn main() {
     println!("Hello, world!");
 
 }
 
-#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
-struct Point {
-    x: i32,
-    y: i32
-}
-
-impl Point {
-    fn origin() -> Point {
-        Point { x: 0, y: 0 }
-    }
-
-    fn translate(&mut self, x: i32, y: i32) {
-        self.x += x;
-        self.y += y;
-    }
-}
-
 #[derive(Debug)]
 struct Solution {
     file: &'static str,
-    rope: Vec<Point>,
-    tail_visits: HashSet<Point>,
+    add_register: VecDeque<i32>
 }
 
 impl Solution {
     
     fn origin(file: &'static str) -> Solution {
-        let mut tail_visits = HashSet::new();
         Solution { 
             file,
-            rope: vec![
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }
-            ],
-            tail_visits 
+            add_register: VecDeque::from([0])
         }
     }
 
-    fn solve(&mut self, file: &str) -> usize {
+    fn solve(&mut self, file: &str) -> i32 {
 
         let contents = fs::read_to_string(file)
             .expect("Should have been able to read the file");
     
         let lines = contents.lines();
 
-        
-        self.tail_visits.insert(self.rope[9]);
+
+        let mut cycle: i32 = 0;
+        let mut add_register = 1;
+        let mut signal_strength = 0;
 
         for line in lines {
+            // cycle += 1;
+
+            if (cycle - 20) % 40 == 0 {
+                println!("Adding to signal strength: {add_register} on cycle: {cycle}");
+                signal_strength += add_register * cycle;
+            }
+
             let mut instructions = line.split_whitespace();
-            let direction: &str = instructions.next().expect("No direction was provided");
-            let delta: i32 = instructions
-                .next()
-                .expect("No delta value was provided")
-                .parse()
-                .unwrap();
+            let instruction: &str = instructions.next().expect("No instruction was provided");
             
-            for _step in 0..delta {
-                self.move_head(direction);
+            let mut delta = 0;
+
+
+            if instruction != "noop" {            
+                delta = instructions
+                    .next()
+                    .expect("no amount provided")
+                    .parse()
+                    .unwrap();
+            }
+
+            println!("Cycle: {cycle} Add register: {add_register}");
+
+            match instruction {
+                "noop" => {
+                    self.add_register.push_back(0);
+                    if !self.add_register.is_empty() {
+                        let value = self.add_register.pop_front().expect("No value found in add register queue");
+                        if value != 0 {
+                            cycle += 1;
+                            
+                            if (cycle - 20) % 40 == 0 {
+                                println!("Adding to signal strength: {add_register} on cycle: {cycle} noop");
+                                signal_strength += add_register * cycle;
+                            }
+                            add_register += value;
+                        }
+                    }
+                    cycle += 1;
+                    
+                },
+                "addx" => {
+                    if !self.add_register.is_empty() {
+                        let value = self.add_register.pop_front().expect("No value found in add register queue");
+                        if value != 0 {
+                            cycle += 1;
+                            
+                            if (cycle - 20) % 40 == 0 {
+                                println!("Adding to signal strength: {add_register} on cycle: {cycle}");
+                                signal_strength += add_register * cycle;
+                            }
+                            add_register += value;
+                        }
+                    }
+                    println!("Adding to register: {delta}");
+                    self.add_register.push_back(delta);
+                    cycle += 1;
+                },
+                _ => println!("unsupported instruction"),
             }
         }
     
-        println!("Head: {:?}", self.rope[0]);
-        println!("Tail: {:?}", self.rope[9]);
-        let answer = self.tail_visits.len();
-        answer
+        signal_strength
     }
 
-    fn move_head(&mut self, direction: &str) {
-        match direction {
-            "R" => self.rope[0].translate(1, 0),
-            "U" => self.rope[0].translate(0, 1),
-            "L" => self.rope[0].translate(-1, 0),
-            "D" => self.rope[0].translate(0, -1),
-            _ => {}
-        }
-        
-        self.move_tail(1);
-    }
 
-    fn move_tail(&mut self, index: usize) {
-        let x_delta = self.rope[index].x - self.rope[index - 1].x;
-        let y_delta = self.rope[index].y - self.rope[index - 1].y;
-        match (x_delta, y_delta) {
-            // same row
-            (-2, 0) => self.rope[index].translate(1, 0),
-            (2, 0) => self.rope[index].translate(-1, 0),
-            (0, -2) => self.rope[index].translate(0, 1),
-            (0, 2) => self.rope[index].translate(0, -1),
-            // diagonals
-            (1, 2) => self.rope[index].translate(-1, -1),
-            (1, -2) => self.rope[index].translate(-1, 1),
-            (-1, 2) => self.rope[index].translate(1, -1),
-            (-1, -2) => self.rope[index].translate(1, 1),
-            (2, 1) => self.rope[index].translate(-1, -1),
-            (-2, 1) => self.rope[index].translate(1, -1),
-            (2, -1) => self.rope[index].translate(-1, 1),
-            (-2, -1) => self.rope[index].translate(1, 1),
-            (2, 2) => self.rope[index].translate(-1, -1),
-            (-2, 2) => self.rope[index].translate(1, -1),
-            (2, -2) => self.rope[index].translate(-1, 1),
-            (-2, -2) => self.rope[index].translate(1, 1),
-            _ => {}
-        }
-        
-        let rope_moved = (self.rope[index].x - self.rope[index - 1].x).abs() < x_delta.abs() || (self.rope[index].y - self.rope[index - 1].y).abs() < y_delta.abs();
-        if rope_moved {
-            // println!("moved rope section {index}");
-            if index == 9 {
-                self.tail_visits.insert(self.rope[index]);
-            } else {
-                self.move_tail(index + 1);
-            }
-        }
-    }
 }
 
 
@@ -140,51 +111,19 @@ mod tests {
     fn test_input() {
         let mut solution = Solution {
             file: "src/test-case-input.txt",
-            rope: vec![
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }
-            ],
-            tail_visits: HashSet::new()
+            add_register: VecDeque::from([0])
         };
         let answer = solution.solve(solution.file);
-        assert_eq!(answer, 36);
+        assert_eq!(answer, 13140);
     }
 
     #[test]
     fn test_solution() {
         let mut solution = Solution {
             file: "src/input.txt",
-            rope: vec![
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }, 
-                Point { x: 0, y:0 }
-            ],
-            tail_visits: HashSet::new()
+            add_register: VecDeque::from([0])
         };
         let answer = solution.solve(solution.file);
         println!("{answer}")
-    }
-
-    #[test]
-    fn test_translate() {
-        let mut point = Point { x: 0, y: 0 };
-        point.translate(1, 1);
-        assert_eq!(point.x, 1);
-        assert_eq!(point.x, 1);
     }
 }
