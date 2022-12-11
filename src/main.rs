fe6e23b511862c228a068b24c2cecb0f5e4a2374
Christopher Guilliams <chrisg07@vt.edu>
2022-12-11 01:23:18 -0600

    Day 10 Part B

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::{HashSet, VecDeque}};
+use std::{fs, collections::VecDeque, ops::{Add, Sub, Rem, Div}};
 
 fn main() {
     println!("Hello, world!");
@@ -8,7 +8,8 @@ fn main() {
 #[derive(Debug)]
 struct Solution {
     file: &'static str,
-    add_register: VecDeque<i32>
+    add_register: VecDeque<f64>,
+    pixels: String
 }
 
 impl Solution {
@@ -16,7 +17,8 @@ impl Solution {
     fn origin(file: &'static str) -> Solution {
         Solution { 
             file,
-            add_register: VecDeque::from([0])
+            add_register: VecDeque::from([0_f64]),
+            pixels: String::from("................................................................................................................................................................................................................................................"),
         }
     }
 
@@ -28,14 +30,14 @@ impl Solution {
         let lines = contents.lines();
 
 
-        let mut cycle: i32 = 0;
-        let mut add_register = 1;
-        let mut signal_strength = 0;
+        let mut cycle: f64 = 0.0;
+        let mut add_register: f64 = 1.0;
+        let mut signal_strength: f64 = 0.0;
 
         for line in lines {
             // cycle += 1;
 
-            if (cycle - 20) % 40 == 0 {
+            if cycle.sub(20.0).rem(40.0) == 0.0 {
                 println!("Adding to signal strength: {add_register} on cycle: {cycle}");
                 signal_strength += add_register * cycle;
             }
@@ -58,50 +60,58 @@ impl Solution {
 
             match instruction {
                 "noop" => {
-                    self.add_register.push_back(0);
-                    if !self.add_register.is_empty() {
-                        let value = self.add_register.pop_front().expect("No value found in add register queue");
-                        if value != 0 {
-                            cycle += 1;
-                            
-                            if (cycle - 20) % 40 == 0 {
-                                println!("Adding to signal strength: {add_register} on cycle: {cycle} noop");
-                                signal_strength += add_register * cycle;
-                            }
-                            add_register += value;
-                        }
-                    }
-                    cycle += 1;
+                    self.add_register.push_back(0_f64);
+                    self.fun_name(&mut cycle, &mut signal_strength, &mut add_register);
                     
                 },
                 "addx" => {
-                    if !self.add_register.is_empty() {
-                        let value = self.add_register.pop_front().expect("No value found in add register queue");
-                        if value != 0 {
-                            cycle += 1;
-                            
-                            if (cycle - 20) % 40 == 0 {
-                                println!("Adding to signal strength: {add_register} on cycle: {cycle}");
-                                signal_strength += add_register * cycle;
-                            }
-                            add_register += value;
-                        }
-                    }
+                    self.fun_name(&mut cycle, &mut signal_strength, &mut add_register);
                     println!("Adding to register: {delta}");
-                    self.add_register.push_back(delta);
-                    cycle += 1;
+                    self.add_register.push_back(delta as f64);
                 },
                 _ => println!("unsupported instruction"),
             }
         }
     
-        signal_strength
+        println!("{:?}", self.pixels);
+        signal_strength as i32
     }
 
+    fn fun_name(&mut self, cycle: &mut f64, signal_strength: &mut f64, add_register: &mut f64) {
+        if !self.add_register.is_empty() {
+            let value = self.add_register.pop_front().expect("No value found in add register queue");
+            if value != 0.0 {
+                self.draw_pixel(*cycle, *add_register as i32);
+                *cycle = cycle.add(1.0);
+                self.draw_pixel(*cycle, *add_register as i32);
+                if cycle.sub(20.0).rem(40.0) == 0.0 {
+                    println!("Adding to signal strength: {add_register} on cycle: {cycle}");
+                    *signal_strength += *add_register * *cycle;
+                }
+                *add_register += value;
+            }
+        }
+        *cycle = cycle.add(1.0);
+        self.draw_pixel(*cycle, *add_register as i32);
+    }
 
+    fn draw_pixel(&mut self, cycle: f64, x: i32) {
+        let row = cycle.div(40.0).floor() as i32;
+        
+        let pixel = cycle.rem(40.0) -1.0;
+
+        let pixel_should_be_drawn = pixel == (x - 1).into() || pixel == x.into() || pixel == (x + 1).into();
+
+        if pixel_should_be_drawn {
+            let range = (cycle as usize - 1)..cycle as usize;
+            self.pixels.replace_range(range, "#");
+
+    
+        }
+        println!("Drawing pixel on row: {row} and column: {pixel}");
+
+    }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -111,7 +121,9 @@ mod tests {
     fn test_input() {
         let mut solution = Solution {
             file: "src/test-case-input.txt",
-            add_register: VecDeque::from([0])
+            add_register: VecDeque::from([0_f64]),
+            pixels: String::from("................................................................................................................................................................................................................................................"),
+
         };
         let answer = solution.solve(solution.file);
         assert_eq!(answer, 13140);
@@ -121,7 +133,9 @@ mod tests {
     fn test_solution() {
         let mut solution = Solution {
             file: "src/input.txt",
-            add_register: VecDeque::from([0])
+            add_register: VecDeque::from([0_f64]),
+            pixels: String::from("................................................................................................................................................................................................................................................"),
+
         };
         let answer = solution.solve(solution.file);
         println!("{answer}")
